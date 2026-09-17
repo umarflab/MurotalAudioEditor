4d6942e04586c13de78961998a898f05d11c89a3
umarflab <umarfworks@gmail.com>
2026-09-18 01:03:44 +0700

    fix: import Compose activity content API

--- app/src/main/java/id/umarflab/murotalaudioeditor/MainActivity.kt
+++ app/src/main/java/id/umarflab/murotalaudioeditor/MainActivity.kt
@@ -3,6 +3,7 @@ package id.umarflab.murotalaudioeditor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
